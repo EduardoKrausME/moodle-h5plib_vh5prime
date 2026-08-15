29ec93e4cdf68b03b2c6ca953f6e432e546ca211
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-14 23:14:20 -0300

    V 1.0.12

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2026081400;
-$plugin->release = "1.0.11";
+$plugin->release = "1.0.12";
 $plugin->requires = 2024100100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'h5plib_vh5prime';
