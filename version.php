037fa4218650c80827adba2a618855d77ae06df8
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-14 22:28:22 -0300

    Expected MOODLE_INTERNAL check or config.php inclusion

--- version.php
+++ version.php
@@ -22,6 +22,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die;
+
 $plugin->version = 2026081400;
 $plugin->release = "1.0.7";
 $plugin->requires = 2024100100;
